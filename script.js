af066406614dfec3f8f35d24360508bc3e0deb7f
Kavin Raj <agkavinraj24@gmail.com>
2021-11-02 10:20:36 +0530

    final

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-form_validate(){
-    let name = document.forms[animi][name].value;
-    document.getElementById("Demo").innerHTML=name;
-    alert("Name Must be filled" + name);
+function form_validate(){
+    var special_char = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    let name = document.forms["animi"]["name"].value;
+    let epi = document.forms["animi"]["episodes"].value;
     if (name == "")
     {
-      
+        alert("Empty");
         return false;
     }
     else if(name.length < 8)
+    {
+        alert("length less than 8");
         return false;
+    }
     else if(name.length >15)
+    {
+        alert("Size greater than 15");
         return false;
+    }    
     else if(name.indexOf(" ") >0)
+    {
+        alert("Has Spaces");
         return false;
+    }
+    else if(special_char.test(name))
+    {
+        alert("Enter name without special characters");
+        return false;
+    }
 }
